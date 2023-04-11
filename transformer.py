0e57a63cf559590c4c8c55dbac3b753b52928f40
Dominick Johnson <dominick.johnson@tylertech.com>
2023-04-11 09:10:48 -0600

    Make it actually work, more or less

--- transformer.py
+++ transformer.py
@@ -11,9 +11,9 @@ from pdfminer.utils import decode_text
 from bitarray import bitarray
 from dataclasses import dataclass, field as datafield
 from enum import Enum
-from typing import List, Any
+from typing import Iterable, Any
 
-def main(input_path, output_path, *, pdf2html, pdf2html_options=[], no_scripts=False, no_ui=False, do_form=False):
+def main(input_path, output_path, *, pdf2html, pdf2html_options=[], no_scripts=False, no_ui=False, do_form=False, zoom=1):
     if not os.path.isfile(pdf2html):
         print("Cannot run pdf2html; path is not valid", file=sys.stderr)
         exit(1)
@@ -38,7 +38,7 @@ def main(input_path, output_path, *, pdf2html, pdf2html_options=[], no_scripts=F
                 el.decompose()
         
         if do_form:
-            _process_form(input_path, soup)
+            _process_form(input_path, soup, zoom)
         
         file.seek(0)
         file.write(str(soup))
@@ -51,8 +51,20 @@ def _decode_pdf_value(value):
         value = decode_text(value)
     return value
 
+
+def process_form_to_trees(pdf_filename: str):
+    from json import dumps
+    with open(pdf_filename, 'rb') as pdf_file:
+        parser = PDFParser(pdf_file)
+        doc = PDFDocument(parser)
+        res = resolve1(doc.catalog)
+        if 'AcroForm' not in res:
+            return # No form found, so nothing to do
+        fields = resolve1(doc.catalog['AcroForm'])['Fields']  # may need further resolving
+        return list(_iter_pdf_form_fields(fields))
+
 # See https://pdfminersix.readthedocs.io/en/latest/howto/acro_forms.html
-def _process_form(pdf_filename: str, soup: BeautifulSoup):
+def _process_form(pdf_filename: str, soup: BeautifulSoup, zoom: int = 1):
     from json import dumps
     with open(pdf_filename, 'rb') as pdf_file:
         parser = PDFParser(pdf_file)
@@ -62,7 +74,7 @@ def _process_form(pdf_filename: str, soup: BeautifulSoup):
             return # No form found, so nothing to do
         html_form = soup.find(id='page-container').wrap(soup.new_tag('form'))
         html_pages = html_form.find_all(class_='pf')
-        inputs_by_parent = defaultdict(lambda: soup.new_tag('div', class_='form-inputs'))
+        inputs_by_parent = defaultdict(lambda: soup.new_tag('div', attrs={'class':'form-inputs'}))
         fields = resolve1(doc.catalog['AcroForm'])['Fields']  # may need further resolving
         for field in _iter_pdf_form_fields(fields):
             if field.type == FormFieldType.button:
@@ -77,40 +89,60 @@ def _process_form(pdf_filename: str, soup: BeautifulSoup):
                 continue
             input.attrs['name'] = field.map_name
             input.attrs['aria-label'] = field.alt_name
+            # The PDF format considers the bottom-left corner to be the origin
             input.attrs['style'] = f"""
                 position: absolute;
-                left: {field.rect.left}px;
-                right: {field.rect.right}px;
-                top: {field.rect.top}px;
-                bottom: {field.rect.bottom}px;
-            """.replace('\n', '').replace(' ', '')
-            input.attrs['data-details'] = repr(field)
+                left: {field.rect.x1 * zoom}px;
+                bottom: {field.rect.y1 * zoom}px;
+                width: {(field.rect.x2 - field.rect.x1) * zoom}px;
+                height: {(field.rect.y2 - field.rect.y1) * zoom}px;
+            """.replace('                ', ' ').replace('\n', '')
+            inputs_by_parent[field.raw_data['P'].objid].append('\n')
             inputs_by_parent[field.raw_data['P'].objid].append(input)
+            inputs_by_parent[field.raw_data['P'].objid].append('\n')
         for fieldset, page in zip(inputs_by_parent.values(), html_pages):
             page.append(fieldset)
+    style = soup.new_tag('style')
+    style.append("""
+        .form-inputs{
+            bottom: -3px;
+            left: -4px;
+            position: absolute;
+        }
+        .form-inputs input,
+        .form-inputs select{
+            border: none;
+            background: rgba(0,0,0,.05);
+        }
+    """)
+    soup.find('head').append(style)
 
 
 class FormFieldType(Enum):
-    none = None
     button = 'Btn'
     text = 'Tx'
     choice = 'Ch'
     signature = 'Sig'
 
-@dataclass
 class FormField:
+    # see https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/PDF32000_2008.pdf
     parent: FormField
-    children: List[FormField] = datafield(init=False, repr=False)
-    name: str
-    values: Any
-    default: Any
-    type: str = datafield(repr=False)
-    alt_name: str = datafield(repr=False)
-    map_name: str = datafield(repr=False)
-    annotation_flags: bitarray = datafield(repr=False)
-    field_flags: bitarray = datafield(repr=False)
-    rect: Rect = datafield(repr=False)
-    raw_data: dict  = datafield(repr=False)
+    raw_data: dict
+
+    def __init__(self, raw_data:dict, parent:FormField=None) -> None:
+        self.raw_data = raw_data
+        self.parent = parent
+        self._values = self._default_values = self._field_flags = self._annotation_flags = self._rect = self._children = None
+    
+    @property
+    def name(self):
+        return _decode_pdf_value(self.raw_data.get('T'))
+    @property
+    def alt_name(self):
+        return _decode_pdf_value(self.raw_data.get('TU')) or self.name
+    @property
+    def map_name(self):
+        return _decode_pdf_value(self.raw_data.get('TM')) or self.name
 
     # TODO not sure I have the flag index right here
     @property
@@ -122,51 +154,98 @@ class FormField:
     @property
     def flag_no_export(self):
         return self.field_flags[2]
+    @property
+    def type(self):
+        if 'FT' in self.raw_data:
+            return FormFieldType(_decode_pdf_value(self.raw_data['FT']))
+        # Type can be inherited from parent
+        if self.parent is not None:
+            return self.parent.type
+        return None
+    @property
+    def values(self):
+        if self._values is not None:
+            return self._values
+        if 'V' in self.raw_data:
+            values = resolve1(self.raw_data.get('V'))
+            if isinstance(values, list):
+                values = [_decode_pdf_value(v) for v in values]
+            else:
+                values = _decode_pdf_value(values)
+            self._values = values
+            return self._values
+        # Values can also be inherited from parent
+        if self.parent is not None:
+            return self.parent.values
+        return None
+    @property
+    def default_values(self):
+        if self._default_values is not None:
+            return self._default_values
+        if 'DV' in self.raw_data:
+            values = resolve1(self.raw_data.get('DV'))
+            if isinstance(values, list):
+                values = [_decode_pdf_value(v) for v in values]
+            else:
+                values = _decode_pdf_value(values)
+            self._default_values = values
+            return self._default_values
+        # Default values can also be inherited from parent
+        if self.parent is not None:
+            return self.parent.default_values
+        return None
+    @property
+    def field_flags(self) -> bitarray|None:
+        if self._field_flags is not None:
+            return self._field_flags
+        if 'Ff' in self.raw_data:
+            field_flags = self.raw_data['Ff']
+            self._field_flags = bitarray("{0:b}".format(field_flags)) if field_flags else bitarray(32)
+            return self._field_flags
+        # Field flags values can also be inherited from parent
+        if self.parent is not None:
+            return self.parent.field_flags
+        return None
+    @property
+    def annotation_flags(self) -> bitarray|None:
+        if self._annotation_flags is not None:
+            return self._annotation_flags
+        if 'Ff' in self.raw_data:
+            annotation_flags = self.raw_data['F']
+            self._annotation_flags = bitarray("{0:b}".format(annotation_flags)) if annotation_flags else bitarray(32)
+            return self._annotation_flags
+        return None
+    @property
+    def rect(self):
+        if self._rect is not None:
+            return self._rect
+        if 'Rect' in self.raw_data:
+            self._rect = Rect(*self.raw_data.get('Rect'))
+            return self._rect
+        return None
+    @property
+    def children(self):
+        if self._children is not None:
+            return self._children
+        if 'Kids' in self.raw_data:
+            self._children = tuple([FormField(resolve1(child), self) for child in self.raw_data['Kids']])
+            return self._children
+        return None
 
-Rect = namedtuple('Rect', 'left,top,right,bottom')
-
-def _iter_pdf_form_fields(fields, parent=None):
-    for f in fields:
-        field = resolve1(f)
-        print(field)
-        # see https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/PDF32000_2008.pdf
-        name = _decode_pdf_value(field.get('T'))
-        alt_name = _decode_pdf_value(field.get('TU')) or name
-        map_name = _decode_pdf_value(field.get('TM')) or name
-        values = resolve1(field.get('V'))
-        default = resolve1(field.get('DV'))
-        type = FormFieldType(_decode_pdf_value(field.get('FT')))
-        annotation_flags = field.get('F')
-        field_flags = field.get('Ff')
-        style_dict = field.get('Mk') # TODO parse this
-        rect = Rect(*field.get('Rect')) if 'Rect' in field else None
-        # TODO: MaxLen
+    #TODO MaxLen
+    #TODO Mk (style dictionary)
 
 
-        # decode value(s)
-        if isinstance(values, list):
-            values = [_decode_pdf_value(v) for v in values]
+Rect = namedtuple('Rect', 'x1,y1,x2,y2')
+
+def _iter_pdf_form_fields(fields):
+    for field in fields:
+        if isinstance(field, FormField):
+            field_obj = field
         else:
-            values = _decode_pdf_value(values)
-
-        field_obj = FormField(
-            parent,
-            name,
-            values,
-            default,
-            type,
-            alt_name,
-            map_name,
-            bitarray("{0:b}".format(annotation_flags)) if annotation_flags else bitarray(32),
-            bitarray("{0:b}".format(field_flags)) if field_flags else bitarray(32),
-            rect,
-            field
-        )
-        children = field.get('Kids')
-        # Yield only the terminal fields; the rest we can get though the parent attribute
-        if children:
-            field_obj.children = tuple(_iter_pdf_form_fields(children, field_obj))
-            yield from field_obj.children
+            field_obj = FormField(resolve1(field))
+        if field_obj.children is not None:
+            yield from _iter_pdf_form_fields(field_obj.children)
         else:
             yield field_obj
 
@@ -185,18 +264,27 @@ if __name__ == '__main__':
     # passthrough args
     parser.add_argument('--first-page', '-f', type=lambda val: ('-f', val), dest='options', action='append')
     parser.add_argument('--last-page', '-l', type=lambda val: ('-l', val), dest='options', action='append')
-    parser.add_argument('--zoom', type=lambda val: ('--zoom', val), dest='options', action='append')
-    parser.add_argument('--fit-width', type=lambda val: ('--fit-width', val), dest='options', action='append')
-    parser.add_argument('--fit-height', type=lambda val: ('--fit-height', val), dest='options', action='append')
     parser.add_argument('--bg-format', type=lambda val: ('--bg-format', val), dest='options', action='append')
 
+    # indirect passthrough
+    zoom_group = parser.add_mutually_exclusive_group()
+    zoom_group.add_argument('--zoom')
+    zoom_group.add_argument('--fit-width')
+    zoom_group.add_argument('--fit-height')
+
     args = parser.parse_args()
+    indirect_options = []
+    for key in ['zoom', 'fit-width', 'fit-height']:
+        val = getattr(args, key.replace('-', '_'))
+        if val is not None:
+            indirect_options.append((f"--{key}", val))
     main(
         args.pdf, 
         args.output or f"{args.pdf}.html", 
         pdf2html=args.pdf2html, 
-        pdf2html_options = list(chain(*(args.options or []))),
+        pdf2html_options = list(chain(*(args.options or []), *indirect_options)),
         no_scripts=args.no_scripts,
         no_ui=args.no_ui,
         do_form=args.do_form,
+        zoom=int(args.zoom) # todo calculate zoom from fit-width or fit-height too (fit_width / actual_width, fit_height / actual_height)
     )
