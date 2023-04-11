07bb828fa4d90ab110183aae14e4478baccaf4eb
Dominick Johnson <dominick.johnson@tylertech.com>
2023-04-11 15:25:03 -0600

    Attempt at getting different field types working (Failure, but progress...)

--- transformer/forms/__init__.py
+++ transformer/forms/__init__.py
@@ -18,12 +18,11 @@ def process_form_to_trees(pdf_filename: str):
         res = resolve1(doc.catalog)
         if 'AcroForm' not in res:
             return # No form found, so nothing to do
-        fields = resolve1(doc.catalog['AcroForm'])['Fields']  # may need further resolving
+        fields = resolve1(doc.catalog['AcroForm'])['Fields']
         return list(iter_pdf_form_fields(fields))
 
 # See https://pdfminersix.readthedocs.io/en/latest/howto/acro_forms.html
 def process_form(pdf_filename: str, soup: BeautifulSoup, zoom: int = 1):
-    from json import dumps
     with open(pdf_filename, 'rb') as pdf_file:
         parser = PDFParser(pdf_file)
         doc = PDFDocument(parser)
@@ -33,16 +32,29 @@ def process_form(pdf_filename: str, soup: BeautifulSoup, zoom: int = 1):
         html_form = soup.find(id='page-container').wrap(soup.new_tag('form'))
         html_pages = html_form.find_all(class_='pf')
         inputs_by_parent = defaultdict(lambda: soup.new_tag('div', attrs={'class':'form-inputs'}))
-        fields = resolve1(doc.catalog['AcroForm'])['Fields']  # may need further resolving
+        fields = resolve1(doc.catalog['AcroForm'])['Fields']
         for field in iter_pdf_form_fields(fields):
+            # TODO not sure if I have the flag index wrong or what, but the commented-out code below causes issues
             if field.type == FormFieldType.button:
-                input = soup.new_tag('input', type='checkbox')
+                # if field.is_radio_button:
+                #     input = soup.new_tag('input', type='radio')
+                # elif field.is_push_button:
+                #     input = soup.new_tag('button')
+                # else: # field.is_checkbox
+                    input = soup.new_tag('input', type='checkbox')
             elif field.type == FormFieldType.text:
-                input = soup.new_tag('input', type='text')
+                if field.is_multiline_text:
+                    input = soup.new_tag('textarea')
+                # elif field.is_password:
+                #     input = soup.new_tag('input', type='password')
+                # elif field.is_file:
+                #     input = soup.new_tag('input', type='file')
+                else:
+                    input = soup.new_tag('input', type='text')
             elif field.type == FormFieldType.choice:
                 input = soup.new_tag('select')
             elif field.type == FormFieldType.signature:
-                input = soup.new_tag('input', type='file')
+                input = soup.new_tag('input', type='file', attrs={'data-real-type':'signature', 'accept':'image/jpeg'})
             else:
                 continue
             input.attrs['name'] = field.map_name
@@ -68,9 +80,11 @@ def process_form(pdf_filename: str, soup: BeautifulSoup, zoom: int = 1):
             position: absolute;
         }
         .form-inputs input,
+        .form-inputs textarea,
         .form-inputs select{
             border: none;
             background: rgba(0,0,0,.05);
+            resize: none;
         }
     """)
     soup.find('head').append(style)
--- transformer/forms/types.py
+++ transformer/forms/types.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 from pdfminer.pdftypes import resolve1
 from bitarray import bitarray
 from enum import Enum
-from typing import Iterable, Any
+from typing import Iterable, Any, List, Tuple
 from .utils import decode_pdf_value
 
 
@@ -35,16 +35,102 @@ class FormField:
     def map_name(self) -> str|None:
         return decode_pdf_value(self.raw_data.get('TM')) or self.name
 
-    # TODO not sure I have the flag index right here
+    # Note: all flag indexes should be the inverse of the value in the PDF documentation
     @property
-    def flag_read_only(self) -> bool:
-        return self.field_flags[0]
+    def is_read_only(self) -> bool:
+        return self.field_flags[-1]
     @property
-    def flag_required(self) -> bool:
-        return self.field_flags[1]
+    def is_required(self) -> bool:
+        return self.field_flags[-2]
     @property
-    def flag_no_export(self) -> bool:
-        return self.field_flags[2]
+    def is_no_export(self) -> bool:
+        return self.field_flags[-3]
+    @property
+    def is_radio_button_no_toggle_off(self) -> bool:
+        """For radio buttons, indicates that once set, the value cannot be unselected. (This behavior is the only supported behavior in HTML)"""
+        return self.type == FormFieldType.button and self.field_flags[-15]
+    @property
+    def is_radio_button_in_unison(self) -> bool:
+        """For radio buttons, indicates that buttons with the same value are selected "as one". (This behavior is not supported in HTML)"""
+        return self.type == FormFieldType.button and self.field_flags[-26]
+    @property
+    def is_radio_button(self) -> bool:
+        """For buttons, indicates if this is a radio button"""
+        return self.type == FormFieldType.button and self.field_flags[-16]
+    @property
+    def is_push_button(self) -> bool:
+        """For buttons, indicates if this is a regular button (push button)"""
+        return self.type == FormFieldType.button and self.field_flags[-17]
+    @property
+    def is_checkbox(self) -> bool:
+        """For buttons, indicates if this is a checkbox"""
+        return self.type == FormFieldType.button and not (self.field_flags[-16] or self.field_flags[-17])
+    @property
+    def is_multiline_text(self) -> bool:
+        """For text fields, indicates that this is a multiline field"""
+        return self.type == FormFieldType.text and self.field_flags[-13]
+    @property
+    def is_password(self) -> bool:
+        """For text fields, indicates that this is a password field"""
+        return self.type == FormFieldType.text and self.field_flags[-14]
+    @property
+    def is_file(self) -> bool:
+        """For text fields, indicates that this is a file upload field"""
+        return self.type == FormFieldType.text and self.field_flags[-21]
+    @property
+    def spellcheck_allowed(self) -> bool:
+        """For text or choice fields, indicates that this is a field allows spellchecking"""
+        return self.type in (FormFieldType.text, FormFieldType.choice) and not self.field_flags[-23]
+    @property
+    def scroll_allowed(self) -> bool:
+        """For text fields, indicates that this field allows more text than fits in it's defined area (This is the default behavior in HTML, so this flag is meaningless)"""
+        return self.type == FormFieldType.text and not self.field_flags[-24]
+    @property
+    def is_text_comb(self) -> bool:
+        """For text fields, indicates that this field is a comb field (Meaning somewhat unclear from docs?)"""
+        return self.type == FormFieldType.text and self.field_flags[-25]
+    @property
+    def is_rich_text(self) -> bool:
+        """For text fields, indicates that this field allows rich text"""
+        return self.type == FormFieldType.text and self.field_flags[-26]
+    @property
+    def is_plain_text(self) -> bool:
+        """For text fields, indicates that this field does not allow rich text"""
+        return self.type == FormFieldType.text and not self.field_flags[-26]
+    @property
+    def max_length(self) -> int|None:
+        """For text fields, indicates the max length of the input"""
+        return self.raw_data.get('MaxLen')
+    @property
+    def is_combo_box(self) -> bool:
+        """For choice fields, indicates that this field is a combobox"""
+        return self.type == FormFieldType.choice and self.field_flags[-18]
+    @property
+    def is_list_box(self) -> bool:
+        """For choice fields, indicates that this field is a list box"""
+        return self.type == FormFieldType.choice and not self.field_flags[-18]
+    @property
+    def is_editable_combo_box(self) -> bool:
+        """For choice fields, indicates that this field is a combobox and includes and editable text field"""
+        return self.type == FormFieldType.choice and self.field_flags[-19]
+    @property
+    def is_multi_select(self) -> bool:
+        """For choice fields, indicates that this field is a multiselect"""
+        return self.type == FormFieldType.choice and self.field_flags[-22]
+    @property
+    def select_options(self) -> List[Tuple[str,str]]:
+        """Options for select fields"""
+        values = self.raw_data.get('Opt')
+        if not values:
+            return values
+        options = []
+        for val in values:
+            if isinstance(val, list):
+                options.append(tuple(val))
+            else:
+                options.append((val, val))
+        return options
+    # TODO TI and I values for choice fields
     @property
     def type(self) -> FormFieldType|None:
         if 'FT' in self.raw_data:
@@ -91,21 +177,21 @@ class FormField:
             return self._field_flags
         if 'Ff' in self.raw_data:
             field_flags = self.raw_data['Ff']
-            self._field_flags = bitarray("{0:b}".format(field_flags)) if field_flags else bitarray(32)
+            self._field_flags = bitarray("{0:0>32b}".format(field_flags))
             return self._field_flags
         # Field flags values can also be inherited from parent
         if self.parent is not None:
             return self.parent.field_flags
-        return None
+        return bitarray(32)
     @property
     def annotation_flags(self) -> bitarray|None:
         if self._annotation_flags is not None:
             return self._annotation_flags
         if 'Ff' in self.raw_data:
             annotation_flags = self.raw_data['F']
-            self._annotation_flags = bitarray("{0:b}".format(annotation_flags)) if annotation_flags else bitarray(32)
+            self._annotation_flags = bitarray("{0:0>32b}".format(annotation_flags))
             return self._annotation_flags
-        return None
+        return bitarray(32)
     @property
     def rect(self) -> Rect|None:
         if self._rect is not None:
@@ -123,6 +209,4 @@ class FormField:
             return self._children
         return None
 
-    #TODO MaxLen
     #TODO Mk (style dictionary)
-    #TODO button type (checkbox, radio, etc...)
